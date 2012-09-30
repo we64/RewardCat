bf9128f5126cef4ccf624c4f8fb5e49cd855c5f2
Chang Liu <changliu@Chang-Lius-MacBook-Pro.local>
2012-09-30 15:51:56 -0700

    Initial Commit

---
+++ Rewards/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rewards
+//
+//  Created by Chang Liu on 2012-09-30.
+//  Copyright (c) 2012 TZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rewards/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rewards
+//
+//  Created by Chang Liu on 2012-09-30.
+//  Copyright (c) 2012 TZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
